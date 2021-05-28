81898fc73143498d02a70d897bba357073fe03cb
letranbaosuong <letranbaosuong@gmail.com>
2021-05-28 11:23:42 +0700

    update camera 1

--- mylibrary/src/main/java/com/example/mylibrary/MyShowCamera.java
+++ mylibrary/src/main/java/com/example/mylibrary/MyShowCamera.java
@@ -1,11 +1,19 @@
 package com.example.mylibrary;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MyShowCamera {
-    public static void ShowCamera(Context context){
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        context.startActivity(intent);
+    public static void ShowCamera(Activity activity) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(Objects.requireNonNull(activity).getPackageManager()) != null) {
+            activity.startActivityForResult(intent, 100);
+        } else {
+            Toast.makeText(activity, "Error", Toast.LENGTH_SHORT).show();
+        }
     }
 }
